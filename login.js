74ac793a8d9db3fd407ed9171ab608b950386ef6
leehj04 <leeeehj04@gmail.com>
2023-05-31 01:01:07 +0900

    0531_commit

---
+++ login.js
@@ -0,0 +1,15 @@
+document.getElementById("SignInButton").addEventListener("click", function() {
+    window.location.href = "SignIn.html";
+  });
+
+document.getElementById("SignUpButton").addEventListener("click", function() {
+    window.location.href = "SignUp.html";
+  });
+
+  document.getElementById("SignUpComplete").addEventListener("click", function() {
+    window.location.href = "main.html";
+  });
+  
+  document.getElementById("SignInComplete").addEventListener("click", function() {
+    window.location.href = "main.html";
+  });
