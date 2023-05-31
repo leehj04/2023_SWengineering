c319dfc453a89ad613d8142e706d5dc28a51123c
aqoudayo <hiyeonu0520@gmail.com>
2023-06-01 01:21:03 +0900

    230601_0120

---
+++ login.js
@@ -0,0 +1,22 @@
+/*
+document.getElementById("SignInButton").addEventListener("click", function() {
+  window.location.href = "SignIn.html";
+});
+
+document.getElementById("SignUpButton").addEventListener("click", function() {
+    window.location.href = "SignUp.html";
+  });
+
+  
+window.onload = function() {
+    // 로그인 정보 가져오기
+    var username = document.getElementById("username").value;
+
+    // 가져온 로그인 정보를 메인 화면에 표시
+    if (username) {
+      document.getElementById("username").textContent = username;
+    }
+
+    // 로그인 버튼 클릭 시 로그인 페이지로 이동
+    
+  };*/
